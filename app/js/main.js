21babd5da4081681687fdce8f8843ac7c80b753d
artem-totlaity <artem.totlaity@gmail.com>
2020-07-10 01:47:00 +0300

    added slider

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: '17px',
+        readOnly: true
+    });
+
+    $('.week-product__slider').slick({
+        prevArrow: '<button class="slick-arrow slick-prev">&#xe875;</button>',
+        nextArrow: '<button class="slick-arrow slick-next">&#xe876;</button>'
+    });
+
+    // var mixer = mixitup('.products__inner-box');
+
+});

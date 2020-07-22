391d6cceddbfbb8b7779dea4271dde32a6b71d94
artem-totlaity <artem.totlaity@gmail.com>
2020-07-23 01:26:40 +0300

    added followers feed, statistic, reasons

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,14 @@ $(function () {
         nextArrow: '<button class="slick-arrow slick-next">&#xe876;</button>'
     });
 
+    $('.followers-feed__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true,
+        prevArrow: '<button class="slick-arrow slick-prev">&#xe875;</button>',
+        nextArrow: '<button class="slick-arrow slick-next">&#xe876;</button>'
+    });
+
     var mixer = mixitup('.newest-product__inner-box');
 
 });

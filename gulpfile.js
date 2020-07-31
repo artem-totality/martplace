54352762b30d5b950cea487ac687ecbf3e88b822
artem-totality <artem.totality@gmail.com>
2020-07-31 12:34:25 +0300

    created login form

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('style', function(){
             'node_modules/slick-carousel/slick/slick.css',
             'node_modules/magnific-popup/dist/magnific-popup.css',
             'app/js/jquery.rateyo.css',
-            'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
+            'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -37,7 +38,8 @@ gulp.task('script', function(){
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
             'node_modules/mixitup/dist/mixitup.js',
             'app/js/jquery.rateyo.js',
-            'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
